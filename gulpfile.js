0652cd2fd089b59167615760ad2f7f4f6aafb66b
Simon Kalt <kaltsimon@users.noreply.github.com>
2015-10-20 12:44:39 +0200

    Re-setup project from scratch to fix internet-access on android

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ var yaml = require('gulp-yaml');
 var sourcemaps = require('gulp-sourcemaps');
 
 var paths = {
-  sass: ['./www/**/*.scss'],
+  sass: ['./scss/*.scss'],
   coffee: ['./www/**/*.coffee'],
   yaml: ['./www/**/*.yml']
 };
@@ -27,11 +27,14 @@ gulp.task('yaml', function(done) {
 });
 
 gulp.task('sass', function(done) {
-  gulp.src('./www/sass/**.scss')
-    .pipe(sass({ errLogToConsole: true}))
-    .pipe(rename({ extname: '.css' }))
-    .pipe(concat('style.css'))
-    .pipe(gulp.dest('./www/css'))
+  gulp.src('./scss/*.scss')
+    .pipe(sass())
+    .on('error', sass.logError)
+    .pipe(minifyCss({
+      keepSpecialComments: 0
+    }))
+    .pipe(concat('style.min.css'))
+    .pipe(gulp.dest('./www/css/'))
     .on('end', done);
 });
 
@@ -50,4 +53,24 @@ gulp.task('watch', function() {
   gulp.watch(paths.sass, ['sass']);
   gulp.watch(paths.coffee, ['coffee']);
   gulp.watch(paths.yaml, ['yaml']);
+});
+
+gulp.task('install', ['git-check'], function() {
+  return bower.commands.install()
+    .on('log', function(data) {
+      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
+    });
+});
+
+gulp.task('git-check', function(done) {
+  if (!sh.which('git')) {
+    console.log(
+      '  ' + gutil.colors.red('Git is not installed.'),
+      '\n  Git, the version control system, is required to download Ionic.',
+      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
+      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
+    );
+    process.exit(1);
+  }
+  done();
 });
